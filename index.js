64c16c3634b89b2c09d904dbce6153cb7ee6a752
Phuong-Cat Ngo <phuongcatngo@gmail.com>
2023-03-16 16:16:07 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+function returnFirstTwoDrivers(drivers){
+    const twoDrivers = [];
+    twoDrivers.push(drivers[0]);
+    twoDrivers.push(drivers[1]);
+    return twoDrivers;
+}
+
+function returnLastTwoDrivers(drivers){
+    const twoDrivers = [];
+    twoDrivers.push(drivers[drivers.length-2]);
+    twoDrivers.push(drivers[drivers.length-1]);
+    return twoDrivers;
+}
+
+let selectingDrivers =[returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const fareMultiplier = createFareMultiplier(2);
+const fareQuintupler = createFareMultiplier(5);
+
+function createFareMultiplier(num){
+    return createFareMultiplier => 25;
+}
+
+function fareDoubler(num){
+    return 2*num;
+}
+
+function fareTripler(num){
+    return 3*num;
+}
+
+function selectDifferentDrivers(drivers, returnFirstTwoDrivers){
+    return returnFirstTwoDrivers(drivers);
+}
